993e7cba651c0663d89b60a7fcab0a09a5e9e673
petruninevgeniy <petruninevgeniy@gmial.com>
2016-03-05 00:29:37 +0200

    hp

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -14,23 +14,46 @@ var SliderSingle = function (obj) {
     var _self = this,
         _obj = obj,
         _sliderSwiper,
-        _slider = _obj.find('.swiper-container');
+        _slider = _obj.find('.swiper-container'),
+        _window = $( window );
 
     //private methods
-    var _initSlider = function () {
+    var _onEvents = function () {
+
+            _window.on( {
+                'load': function () {
+
+                    _initSlider();
+
+                }
+            } );
+
+        },_initSlider = function () {
 
             _sliderSwiper = new Swiper(_slider, {
 
                 pagination: '.swiper-pagination',
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
                 paginationClickable: true,
-                direction: 'vertical'
+                mousewheelControl: true,
+                direction: 'vertical',
+                effect: 'coverflow',
+                coverflow: {
+                    rotate: 90,
+                    stretch: 0,
+                    depth: 300,
+                    modifier: 1,
+                    slideShadows : true
+                },
+                speed: 500
 
             });
 
         },
         _init = function () {
 
-            _initSlider();
+            _onEvents();
             _obj[0].obj = _self;
 
         };
