536fd41d57f91e1e7e6ce239cde477f629670d39
petruninevgeniy <petruninevgeniy@gmial.com>
2016-03-07 14:10:30 +0200

    creat preloader

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -1,4 +1,42 @@
-"use strict";
-( function(){
+$(function () {
+    $.each($( '.preloader' ), function () {
 
-} )();
+        new Preloader($(this));
+
+    });
+});
+
+var Preloader = function ( obj ) {
+
+    var _obj = obj,
+        _deelay = _obj.data( 'deelay' ),
+        _window = $( window);
+
+    var _onEvents = function () {
+
+            _window.on({
+                load: function(){
+
+                    setTimeout(function () {
+                        _obj.addClass( 'hide' );
+
+                        setTimeout(function () {
+
+                            _obj.remove()
+
+                        },400);
+
+                    }, _deelay );
+
+                }
+            });
+
+        },
+
+        _init = function () {
+            _onEvents();
+        };
+
+    _init();
+};
+
